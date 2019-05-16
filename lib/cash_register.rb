764dc70c8aa9d6aad40bcb9ac43a48d75ea7159c
AustinBH <harlowaustin2@gmail.com>
2019-05-16 16:53:09 -0700

    Done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,10 +6,23 @@ class CashRegister
   def initialize(discount=0)
     @total = 0
     @discount = discount
+    @items = []
+    @last_transaction = []
   end
 
   def add_item(title, price, quantity=1)
+    @last_transaction << title
+    @last_transaction << price*quantity
+    @last_transaction << quantity
     @total += price*quantity
+    if quantity == 1
+      @items << title
+    else
+      while quantity > 0
+        @items << title
+        quantity -= 1
+      end
+    end
   end
 
   def apply_discount
@@ -22,11 +35,15 @@ class CashRegister
   end
 
   def items
-    
+    @items
   end
 
   def void_last_transaction
-
+    if @last_transaction.include?(@items.pop)
+      @total -= @last_transaction[1]
+    else
+      "There are no previous transactions"
+    end
   end
 
 end
