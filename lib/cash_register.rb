e9d632884e46c5b237465ab1e576b1c3ef5e9fea
AustinBH <harlowaustin2@gmail.com>
2019-05-15 23:47:50 -0700

    still working through last two tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+require 'pry'
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += price*quantity
+  end
+
+  def apply_discount
+    if @discount > 0
+      @total = @total - (@total*@discount/100)
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    
+  end
+
+  def void_last_transaction
+
+  end
+
+end
